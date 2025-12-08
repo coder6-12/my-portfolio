1f5cfd814939bc2d3d0537d08a9829aec16cbaf9
coder6-12 <netfly933@gmail.com>
2025-12-08 13:21:18 +0530

    home page changes

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -49,9 +49,6 @@ function Home() {
                 Hi, I'm Madhav, a passionate web developer dedicated to creating user-friendly digital experiences.
               </p>
               <div className="hero-buttons">
-                {/* <Link to="/contact" className="btn-primary">
-                  Get In Touch
-                </Link> */}
                 <button onClick={handleDownloadCV} className="btn-primary">
                   Resume
                 </button>
