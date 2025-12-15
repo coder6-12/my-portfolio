679cd602c0b732a1b820a6da035c2093d0780c6f
coder6-12 <netfly933@gmail.com>
2025-12-15 15:36:07 +0530

    added scroll to top always functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/footer/footer"
 import Home from "./pages/Home/Home";
@@ -6,10 +6,20 @@ import Projects from "./pages/Project/Project";
 import About from "./pages/About/About";
 import Contact from "./pages/ContactUs/ContactUs";
 import "./App.css";
+import { useEffect } from "react";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <div className="app">
         <Header />
         <main>
